60b52030e02063b5128c0b97d4d6b448ea00270d
modestekhadija <modestekhadija@gmail.com>
2023-03-02 16:46:44 +0100

    première sauvegarde

---
+++ recommandationapp/views.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    if request.method == 'GET':
+        # Get user_id entered by user in the dashboard
+        user_id = request.args.get('user_id')
+        print('user_id: ', user_id)
+        url = "https://recommandation-system-function.azurewebsites.net/api/based-content-function"
+        recommandations = requests.get(url, params={'user_id': user_id}).json()['recommandation_articles']
+    return render_template('result.html', user_id=user_id, recommandations=recommandations)
