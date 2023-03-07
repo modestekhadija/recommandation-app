fe0c9d8674b308bbbd8873e15a4255bfa0499d9b
modestekhadija <119693964+modestekhadija@users.noreply.github.com>
2023-03-07 21:20:26 +0100

    update last url function app

--- recommandationapp/views.py
+++ recommandationapp/views.py
@@ -15,6 +15,6 @@ def result():
         # Get user_id entered by user in the dashboard
         user_id = request.args.get('user_id')
         print('user_id: ', user_id)
-        url = "https://bc-function.azurewebsites.net/api/based-content-function"
+        url = "https://based-content.azurewebsites.net/api/based-content-function"
         recommandations = requests.get(url, params={'user_id': user_id}).json()['recommandation_articles']
     return render_template('result.html', user_id=user_id, recommandations=recommandations)
